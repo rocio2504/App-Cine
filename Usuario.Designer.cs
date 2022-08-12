b25df6e833f669cacd36f5a8b101fc1180791ac8
rocio2504 <rocioandrea250489@gmail.com>
2022-08-12 17:56:01 -0500

    Updating Usuario class

---
+++ Usuario.Designer.cs
@@ -0,0 +1,253 @@
+﻿
+namespace Login_cine
+{
+    partial class Usuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.ttmensaje1 = new System.Windows.Forms.ToolTip(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Vineta BT", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(230, 41);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Usuarios";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1415, 105);
+            this.panel1.TabIndex = 2;
+            // 
+            // button7
+            // 
+            this.button7.BackgroundImage = global::Login_cine.Properties.Resources.lupa;
+            this.button7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button7.Location = new System.Drawing.Point(1223, 53);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(56, 36);
+            this.button7.TabIndex = 9;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::Login_cine.Properties.Resources.descarga;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(1346, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(51, 33);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(922, 53);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(284, 34);
+            this.textBox1.TabIndex = 8;
+            // 
+            // button5
+            // 
+            this.button5.BackgroundImage = global::Login_cine.Properties.Resources.boton_pdf;
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button5.Location = new System.Drawing.Point(631, 33);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 54);
+            this.button5.TabIndex = 6;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImage = global::Login_cine.Properties.Resources.Btn_delete;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button4.Location = new System.Drawing.Point(525, 33);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 54);
+            this.button4.TabIndex = 5;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(758, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 38);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Buscar:";
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = global::Login_cine.Properties.Resources.agregar_usuario2;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Location = new System.Drawing.Point(313, 33);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 54);
+            this.button2.TabIndex = 3;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = global::Login_cine.Properties.Resources.boton_eliminar;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.Location = new System.Drawing.Point(419, 33);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 54);
+            this.button3.TabIndex = 4;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(41, 146);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(1215, 609);
+            this.dataGridView1.TabIndex = 34;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(122, 813);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(181, 30);
+            this.textBox2.TabIndex = 36;
+            // 
+            // button6
+            // 
+            this.button6.BackgroundImage = global::Login_cine.Properties.Resources.actualizar;
+            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button6.Location = new System.Drawing.Point(1284, 609);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(78, 88);
+            this.button6.TabIndex = 35;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(41, 160);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView2.Size = new System.Drawing.Size(1253, 151);
+            this.dataGridView2.TabIndex = 37;
+            // 
+            // Usuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1415, 875);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Location = new System.Drawing.Point(250, 70);
+            this.Name = "Usuario";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Usuario";
+            this.Load += new System.EventHandler(this.Usuario_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ToolTip ttmensaje1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.DataGridView dataGridView2;
+    }
+}
---
+++ Usuario.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+
+namespace Login_cine
+{
+    public partial class Usuario : Form
+    {
+       
+        SQLControl sqlControl = new SQLControl();
+        public Usuario()
+        {
+            InitializeComponent();
+
+            this.ttmensaje1.SetToolTip(this.button2, "AGREGAR");
+            this.ttmensaje1.SetToolTip(this.button3, "MODIFICAR");
+            this.ttmensaje1.SetToolTip(this.button4, "ELIMINAR");
+            this.ttmensaje1.SetToolTip(this.button5, "REPORTES");
+            this.ttmensaje1.SetToolTip(this.button6, "ACTUALIZAR TABLA");
+            this.ttmensaje1.SetToolTip(this.button7, "BUSCAR");
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // this.Hide();
+            //Ventana2 VentanaMenu = new Ventana2();
+            //VentanaMenu.Show();
+            //este boton es para cerrar
+            this.Close();
+        }
+
+        SqlConnection cnn = new SqlConnection("Data Source =.; Initial Catalog = Cine; Integrated Security = True");
+
+        public void llenar_tabla()
+        {
+            DataTable tabla = new DataTable();
+            tabla = sqlControl.mostrarUsuarios();
+            dataGridView1.DataSource = tabla;
+        }
+
+        private void Usuario_Load(object sender, EventArgs e)
+        {
+            /*  try
+              {
+                  string consulta = "select* from Usuario";
+                  SqlDataAdapter adaptador = new SqlDataAdapter(consulta,cnn);
+                  DataTable dt = new DataTable();
+                  adaptador.Fill(dt);
+                  dataGridView1.DataSource = dt;
+              }
+              catch (Exception exc)
+              {
+                  MessageBox.Show(exc.Message);
+              }*/
+            textBox2.Visible = false;
+            mostrarUsuario();
+            dataGridView2.Visible = false;
+            
+        }
+
+        public void mostrarUsuario()
+        {
+            DataTable tabla = new DataTable();
+            tabla = sqlControl.mostrarUsuarios();
+            dataGridView1.DataSource = tabla;
+        }
+
+        public void buscarUsuario()
+        {
+            DataTable tabla = new DataTable();
+            tabla = sqlControl.buscarUser(textBox1.Text);
+            dataGridView2.DataSource = tabla;
+            dataGridView1.Visible = false;
+            dataGridView2.Visible = true;
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           //este boton es para agregar Usuario
+            AgregarUsuario agregar = new AgregarUsuario();
+            agregar.ShowDialog();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //este boton es para ACTUALIZAR TABLA
+            textBox1.Text = "";
+            dataGridView2.Visible = false;
+            dataGridView1.Visible = true;
+            llenar_tabla();
+
+        }
+                               
+    private void button3_Click(object sender, EventArgs e)
+        {
+            
+            //BOTON PARA EDITAR
+
+            
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+           int n = e.RowIndex;
+           if(n!=-1)
+            {
+               textBox2.Text= dataGridView1.Rows[n].Cells[0].Value.ToString();                             
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+          //string a = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+           //string b = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+          
+         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //boton eliminar
+            // string a = textBox2.Text;
+            if (MessageBox.Show("¿Esta seguro que desea eliminar este registro?", "El Sistema dice:",
+                 MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                string a = sqlControl.eliminarUsuario(textBox2.Text);
+                MessageBox.Show(a);
+                mostrarUsuario();
+            }
+        }
+
+
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            //BOTON BUSCAR
+            buscarUsuario();
+
+        }
+    }
+}
